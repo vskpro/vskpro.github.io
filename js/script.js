96ee1942ddaca4f8522daa9383b4bdab3efcf46a
vskpro <veeranenisaikiran@gmail.com>
2016-10-04 20:08:08 +0530

    Added all the files

---
+++ js/script.js
@@ -0,0 +1,55 @@
+(function($){
+
+	$(document).ready(function(){
+
+		$(".smooth-scroll").on('click', function(e) {
+			// prevent default anchor click behavior
+			e.preventDefault();
+			// animate
+			$('html, body').animate({
+				scrollTop: $(this.hash).offset().top - 100
+			}, 850);
+		});
+
+		
+
+		// cache container
+		var $container = $('#projects-container');
+		// initialize isotope
+		
+  		$(window).load(function() {
+			$container.isotope({});
+		});
+
+
+
+		// filter items when filter link is clicked
+		$('#filters a').click(function(){
+			var selector = $(this).attr('data-filter');
+			$container.isotope({ filter: selector });
+			return false;
+		});
+
+		/*
+
+			Code to highlight current selection
+			1) When any of the links is clicked, listen to the click event
+			2) On click, traverse and remove all the highlight classes
+			3) Add the highlight class to current clicked element	
+
+			*/
+
+			$('.listen-for-click').on('click',function() {
+
+				$('.listen-for-click').removeClass('highlight');
+
+				$(this).addClass('highlight');
+
+
+			});
+
+
+
+	}); //end of document ready
+
+})(jQuery);
